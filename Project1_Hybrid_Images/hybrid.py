8326770c41ca3ad736714b3e83a2c7b6692ac928
Sarah Le Cam <sdl83@cornell.edu>
2018-02-16 12:11:18 -0500

    .

--- Project1_Hybrid_Images/hybrid.py
+++ Project1_Hybrid_Images/hybrid.py
@@ -118,14 +118,15 @@ def gaussian_blur_kernel_2d(sigma, height, width):
 
         for w in range(width):
 
-            x = w - width/2.0
-            y = -h + height/2.0
+            x = w - width/2
+            y = -h + height/2
                 
             # Gaussian distribution
-            kernel[h,w] = 1.0/(2.0 * math.pi * sigma * sigma) * math.exp(-1.0 * (x * x + y * y) / (2.0 * sigma * sigma))
+            kernel[h,w] = 1/(2 * math.pi * sigma * sigma) * math.exp(-1 * (x * x + y * y) / (2 * sigma * sigma))
             
+            sum = np.sum(kernel)
             # nomalizing by dividing by sum
-            normalize = kernel / np.sum(kernel)
+            normalize = kernel / sum
 
     return normalize
 
