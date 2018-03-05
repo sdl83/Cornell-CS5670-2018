3129cc3a9e71e6ee7afd1965ec7f64ccbe07db57
Sarah Le Cam <sdl83@cornell.edu>
2018-03-04 20:26:12 -0500

    fixing error

--- Project2_Feature_Detection/tests.py
+++ Project2_Feature_Detection/tests.py
@@ -132,6 +132,6 @@ try_this('5 and/or 6', MFD.describeFeatures, loaded['f'], compare_array, image, 
 im = MFD.describeFeatures(image, d)
 for i in range(loaded['f'].shape[0]) :
     for j in range(loaded['f'].shape[1]) :
-        if not (loaded['f'][i][j] == im[i][j]: 
+        if not (loaded['f'][i][j] == im[i][j]) : 
             print (i)
 
